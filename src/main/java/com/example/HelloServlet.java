fdd802c3dfce8c92f71cc46e3f7b19f0c39dee68
careelika <careelika@gmail.com>
2014-02-21 14:43:57 +0200

    Just testing Eclipse-Sourcetree-Heroku connection.

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write("Alustamine!".getBytes());
+        out.write("Alustamine! :)".getBytes());
         out.flush();
         out.close();
     }
