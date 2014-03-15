e5e5a7f315bcfddf0a057b4c63a3fd73f6a8e219
JaneJ <jane193@hot.ee>
2014-03-15 21:25:04 +0200

    FB uuesti.

--- src/main/webapp/resources/scripts/logins.js
+++ src/main/webapp/resources/scripts/logins.js
@@ -1,9 +1,9 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 
-  $('#login_button').click(function(){
+  $('#login_fb').click(function(){
     FB.login(function(response) {
    if (response.authResponse) {
-	window.location.href = "index.html";
+	//window.location.href = "index.html";
      console.log('Welcome!  Fetching your information.... ');
      FB.api('/me', function(response) {
        console.log('Good to see you, ' + response.name + '.');
@@ -31,46 +31,8 @@ window.fbAsyncInit = function() {
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-*/
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1385045075103554',
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
 
-  
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-    
-    if (response.status === 'connected') {
-      
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      
-      FB.login();
-    } else {
-      
-      FB.login();
-    }
-  });
-  };
 
-  // Load the SDK asynchronously
-  (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
 
 
 /*SigninCallback: function(authResult){
