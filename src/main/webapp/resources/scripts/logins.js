98e5247201805e92810e341cc5833a6d5c2456f4
JaneJ <jane193@hot.ee>
2014-03-15 19:11:27 +0200

    Google+ login katsetus.

---
+++ src/main/webapp/resources/scripts/logins.js
@@ -0,0 +1,42 @@
+/*$(document).ready(function(){
+
+  $('#login_button').click(function(){
+    FB.login(function(response) {
+   if (response.authResponse) {
+	window.location.href = "index.html";
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ });
+  });
+});
+
+window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1385045075103554',
+          status     : true,
+          xfbml      : true
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));*/
+
+onSigninCallback: function(authResult){
+  if (authResult['access_token']) {
+    $('#gConnect').hide();
+
+    gapi.client.load('plus', 'v1',function(){
+      helper.connect();
+    });
+  }else if (authResult['error']) {}
+}
