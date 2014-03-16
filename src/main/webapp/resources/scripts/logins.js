d96720ada74f6a5ab0a9c69cf782902bc6c4fdb8
JaneJ <jane193@hot.ee>
2014-03-17 00:08:52 +0200

    Midagi läks valesti .

--- src/main/webapp/resources/scripts/logins.js
+++ src/main/webapp/resources/scripts/logins.js
@@ -1,40 +1,85 @@
-
 window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1385045075103554', // App ID
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
-    
-   
-  
-FB.login(function(response) {
-  if (response.authResponse) 
-    {
-      FB.api('/me', function(response) {
-              
-    });
-  } else 
-    {
-      console.log('User cancelled login or did not fully authorize.');
-    }
-     },{scope: 'email,user_photos,user_videos'});
-  
+  FB.init({
+    appId      : '1385045075103554',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
 
- 
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    // Here we specify what we do with the response anytime this event occurs. 
+    if (response.status === 'connected') {
+      
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      
+      FB.login();
+    } else {
+      
+      FB.login();
+    }
+  });
+  };
 
   // Load the SDK asynchronously
   (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document));
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+   FB.logout(function(response) {
+        // Person is now logged out
+    });
 
 
-/*
+
+/*$(document).ready(function(){
+
+  $('#login_fb').click(function(){
+    FB.login(function(response) {
+   if (response.authResponse) {
+	window.location.href = "index.html";
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ });
+  });
+});
+
+window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1385045075103554',
+          status     : true,
+          xfbml      : true
+        });
+      };
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/et_EE/all.js#xfbml=1&appId=1385045075103554";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+
 
 function signinCallback(authResult){
   if (authResult['access_token']) {
