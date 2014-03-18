7f7b357d3044e647c40d131aeefec5a1b61b9a70
JaneJ <jane193@hot.ee>
2014-03-18 16:24:51 +0200

    Google + and FB

--- src/main/webapp/resources/scripts/logins.js
+++ src/main/webapp/resources/scripts/logins.js
@@ -1,4 +1,5 @@
-window.fbAsyncInit = function() {
+//FB  
+  window.fbAsyncInit = function() {
   FB.init({
     appId      : '1385045075103554',
     status     : true, // check login status
@@ -7,7 +8,7 @@ window.fbAsyncInit = function() {
   });
 
   FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // Here we specify what we do with the response anytime this event occurs. 
+    
     if (response.status === 'connected') {
       
       testAPI();
@@ -26,59 +27,18 @@ window.fbAsyncInit = function() {
    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
    if (d.getElementById(id)) {return;}
    js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
+   js.src = "//connect.facebook.net/et_EE/all.js#xfbml=1&appId=1385045075103554";
    ref.parentNode.insertBefore(js, ref);
   }(document));
 
-  // Here we run a very simple test of the Graph API after login is successful. 
-  // This testAPI() function is only called in those cases. 
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Good to see you, ' + response.name + '.');
     });
   }
-   FB.logout(function(response) {
-        // Person is now logged out
-    });
 
-
-/*$(document).ready(function(){
-
-  $('#login_fb').click(function(){
-    FB.login(function(response) {
-   if (response.authResponse) {
-	window.location.href = "index.html";
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-   } else {
-     console.log('User cancelled login or did not fully authorize.');
-   }
- });
-  });
-});
-
-window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '1385045075103554',
-          status     : true,
-          xfbml      : true
-        });
-      };
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/et_EE/all.js#xfbml=1&appId=1385045075103554";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-
+//Google +
 
 function signinCallback(authResult){
   if (authResult['access_token']) {
@@ -93,9 +53,8 @@ function connect(){
   gapi.client.plus.people.get({userID:'me'}).execute{
     function(result){
       helper.user=result;
-      $('#profileArea').hide();
-
       var html=helper.getProfileHTML(result);
+      html+="<input type='button' value='Logout' onclick='Logout();'/>";
         
       $('#profileArea').html('Signed in as' + helper.user.displayName + '!')
       
@@ -108,17 +67,7 @@ getProfileHTML: function(user){
   user.displayName +'" height="35"/>' + '</a>'+user.displayName;
   return html;
 }
-/*function signinCallback(authResult) {
-  if (authResult['status']['signed_in']) {
-    // Update the app to reflect a signed in user
-    // Hide the sign-in button now that the user is authorized, for example:
-    document.getElementById('signinButton').setAttribute('style', 'display: none');
-  } else {
-    // Update the app to reflect a signed out user
-    // Possible error values:
-    //   "user_signed_out" - User is signed-out
-    //   "access_denied" - User denied access to your app
-    //   "immediate_failed" - Could not automatically log in the user
-    console.log('Sign-in state: ' + authResult['error']);
-  }
-}*/
+function Logout(){
+  gapi.auth.signOut();
+}
+
