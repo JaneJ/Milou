b8346d4fdb6c11399aa460ce8d67560e5ee138d5
JaneJ <jane193@hot.ee>
2014-03-19 15:30:40 +0200

    FB login

--- src/main/webapp/resources/scripts/logins.js
+++ src/main/webapp/resources/scripts/logins.js
@@ -1,45 +1,85 @@
-//FB  
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1385045075103554',
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
-
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1385045075103554', // App ID
+      channelUrl : 'http://hayageek.com/examples/oauth/facebook/oauth-javascript/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
     
-    if (response.status === 'connected') {
+    
+  FB.Event.subscribe('auth.authResponseChange', function(response) 
+  {
+   if (response.status === 'connected') 
+    {
+      document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
+      //SUCCESS
       
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      
-      FB.login();
-    } else {
-      
-      FB.login();
+    }  
+  else if (response.status === 'not_authorized') 
+    {
+      document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
+
+    //FAILED
+    } else 
+    {
+      document.getElementById("message").innerHTML +=  "<br>Logged Out";
+
+      //UNKNOWN ERROR
     }
-  });
-  };
+  }); 
+  
+    };
+    
+    function Login()
+  {
+  
+    FB.login(function(response) {
+       if (response.authResponse) 
+       {
+          getUserInfo();
+        } else 
+        {
+           console.log('User cancelled login or did not fully authorize.');
+        }
+     });
+  
+  
+  }
+
+  function getUserInfo() {
+      FB.api('/me', function(response) {
+
+    var str="Tere : "+response.name+"!";
+        str +="<input type='button' id='logoutfb' value='Logout' onclick='Logout();'/>";
+        document.getElementById("profileArea").innerHTML=str;
+              
+    });
+    }
+
+  
+  }
+  function Logout()
+  {
+    FB.logout(function(){document.location.reload();});
+  }
 
   // Load the SDK asynchronously
   (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/et_EE/all.js#xfbml=1&appId=1385045075103554";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
 
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
-   FB.logout(function(response) {
-        // Person is now logged out
-    });*/
+
+/*
+
+
+
+
+
 
 //Google +
 
@@ -73,4 +113,4 @@ getProfileHTML: function(user){
 function Logout(){
   gapi.auth.signOut();
 }
-
+*/
