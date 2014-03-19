1df3fc71eb6e67824db6cfa1e6ff592e831b8b12
Kristiina Pokk <kristiinapokk@gmail.com>
2014-03-19 09:55:41 +0200

    https://github.com/heroku/devcenter-java-database/ näite järgi

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,20 +1,22 @@
 package com.example;
 
+
+/**
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-/**
+
  * 
  * This class launches the web application in an embedded Jetty container.
  * This is the entry point to your application. The Java command that is used for
  * launching should fire this main method.
  *
- */
+ *
 public class Main {
     
     /**
      * @param args
-     */
+     *
     public static void main(String[] args) throws Exception{
         String webappDirLocation = "src/main/webapp/";
         
@@ -46,3 +48,41 @@ public class Main {
     }
 
 }
+*/
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+
+@Component
+public class Main {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) throws Exception {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+    }
+
+    @PostConstruct
+    public void myRealMainMethod() throws SQLException {
+        Statement stmt = dataSource.getConnection().createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+        }
+    }
+
+}
+
+
+
