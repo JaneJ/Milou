8bf2a523556ddd4ff460af5d3f0275945af561fe
careelika <careelika@gmail.com>
2014-03-21 05:27:33 +0200

    Javascript test version.

--- src/main/webapp/resources/js/comment.js
+++ src/main/webapp/resources/js/comment.js
@@ -5,27 +5,39 @@ $(document).ready(function(){
   $('.naitaKommentaare').on( 'click',function(){
   console.log("naitaKommentaare"); 
     $("article").not($(this).closest('article')).hide("slow");
-	$('#uudisKommentaarid').slideDown("slow");
+	$('.uudisKommentaarid').slideDown("slow");
   });
   
   //kui klikitakse kommentaari peal
-  $('.uudisKommentaarid').on( 'click',function(){
-  console.log("#uudisKommentaarid"); 
-	$('#uudisKommentaarid').slideUp("slow");
+  $('.kommenteeriLink').on( 'click',function(){
+  console.log(".uudisKommentaarid"); 
+	$('.uudisKommentaarid').slideDown("slow");
   });
  
   
   //kui valitakse kommenteeri
-  $('.kommenteeri').on( 'click',function(){
-  console.log("#kommenteeri"); 
+  $('.kommenteeriLink').on( 'click',function(){
+  console.log(".uudisKommenteeri"); 
     $("article").not($(this).closest('article')).hide("slow");
-	$('#uudisKommenteeri').slideDown("slow");
+	$(this).closest('.uudisKommenteeri').removeClass(".hidden");
+	
+	$('.uudisKommenteeri').slideDown("slow");
   });
 
-    //kui klikitakse kommenteerimisala peal
-  $('#uudisKommenteeri').on( 'click',function(){
-  console.log("#uudisKommenteeri"); 
-	$('#uudisKommenteeri').slideUp("slow");
-  });
+    /*kui klikitakse kommenteerimisala peal
+  $('.kommenteeriLink').on( 'dblclick',function(){
+  console.log(".uudisKommenteeri"); 
+	$('.uudisKommenteeri').slideUp("slow");
+  });*/
+  
+  	var text_max=300;
+	$('.kommenteeriMargid').html(text_max + ' m&#228;rke alles.' );
+
+	$('.kommenteeriSisu').keyup(function(){
+	console.log("loendab marke");
+		var pealkiri_len=$('.kommenteeriSisu').val().length;
+		var pealkiri_alles=text_max-pealkiri_len;
+		$('.kommenteeriMargid').html(pealkiri_alles+ ' m&#228;rke alles.');
+	});
   
 });
