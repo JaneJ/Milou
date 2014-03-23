46e96a2ca6d7e2b9d9cf6f2a95c438a419ac7d1a
careelika <careelika@gmail.com>
2014-03-23 11:59:37 +0200

    Trying to study some Ajax, irrelevant ajaxTest.

---
+++ src/main/webapp/ajaxTest.js
@@ -0,0 +1,85 @@
+var xmlHttp = createXmlHttpRequestObject();
+
+fuction createXmlHttpRequestObject(){
+	var xmlHttp;
+	
+	if(window.XMLHttpRequest){
+		xmlHttp = new XMLHttpRequest();
+	}else{
+	/*try{*/
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+/*	}catch(e){*/
+	
+	}
+		return xmlHttp;
+	}
+}
+
+function process(){
+	if(xmlHttp){
+		try{
+			xmlHttp.open("GET","ajaxTest.txt", true); /*ei alusta serveriuhendust, konfib uhendust, kuidas ja mida saadetakse*/
+			xmlHttp.onreadystatechange = handleServerResponse;
+			xmlHttp.send(null);
+		
+		}catch(e){
+			alert(e.toString());
+		}
+	}
+}
+
+function handleServerResponse(){
+	theD = document.getElementById('theD');
+	if(xmlHttp.readyState==1){
+		theD.innerHTML += "Status 1: server connection established <br/>";
+	} else if(xmlHttp.readyState==2){
+		theD.innerHTML += "Status 2: request received <br/>";
+	} else if(xmlHttp.readyState==3){
+		theD.innerHTML += "Status 3: processing request <br/>";
+	} else if(xmlHttp.readyState==4){
+		
+		if(xmlHttp.status==200){
+			try[	
+				text = xmlHttp.responseText; /*peaks txt faili sisu olema*/
+				theD.innerHTML += "Status 4: request is finished, response is ready <br/>";
+				theD.innerHTML += text;
+			}catch(e){
+			alert(e.toString());
+		}
+		}else{
+			alert(xmlHttp.statusText);
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
