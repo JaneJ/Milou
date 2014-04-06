19ffd5e48dc77d44148d2e736cd90023911ae3a8
Kristiina Pokk <kristiinapokk@gmail.com>
2014-04-06 14:56:44 +0300

    pildi klassi täiendus

--- src/main/java/ee/ut/math/vl/data/Pilt.java
+++ src/main/java/ee/ut/math/vl/data/Pilt.java
@@ -5,13 +5,18 @@ public class Pilt {
 	public Integer id;
 	public Integer artikkel;
     public byte[] piltBaidis;
+    public String nimi;
+    public String tyyp;
 	
 	
-	 public Pilt(Integer id, Integer artikkel, byte[] piltBaidis) {
+	 public Pilt(Integer id, Integer artikkel, byte[] piltBaidis, String nimi, String tyyp) {
 
 		this.id = id;
 		this.artikkel = artikkel;
 		this.piltBaidis = piltBaidis;
+		this.nimi = nimi;
+		this.tyyp = tyyp;
+		
 	}
 
 
