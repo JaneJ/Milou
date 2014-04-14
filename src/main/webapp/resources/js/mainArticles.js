0e868a15a4a0305676ea8c7d4d6e1c20cb8f07c5
careelika <careelika@gmail.com>
2014-04-14 13:08:49 +0300

    mainArticles.js on click problem solved

--- src/main/webapp/resources/js/mainArticles.js
+++ src/main/webapp/resources/js/mainArticles.js
@@ -65,7 +65,7 @@ $("#sisu").append(vasak,parem);
     }
 
     console.log("koik artiklid tehtud");
-
+    addAside()
 
 
 
@@ -104,6 +104,8 @@ $("#sisu").append(vasak,parem);
             footer.append(p2,loeKom,teeKom);
             article.append(footer);
             article.addClass("article");
+            h1.removeClass("getArticle");
+            h1.addClass("getArticle");
 
            //valib artikli asukohaks kordamooda parem ja vasak div
             if (i%2 ==0)
@@ -119,7 +121,7 @@ $("#sisu").append(vasak,parem);
       };
 
 
-
+       /*
              //kui vajutame artikli pealkirja peal pealehel
              $("h1").on('click',function(){
                  console.log("valiti article,'h1'");
@@ -142,18 +144,21 @@ $("#sisu").append(vasak,parem);
                 showArticle(data)
 
              });
-
+                   */
 
 
                function removeAllButAside(){
-                       $('#sisu').contents(':not(aside)').remove();
+                       //$('#sisu').contents(':not(aside)').remove();
+                       $('#sisu').contents().remove();
                };
+
+
 
 
                 //sarnane buildArticle fun-le, aga ehitab artikli ulest taies suuruses, koos koikide osadega
                function showArticle(json) {   /*argument responseText*/
                     console.log("kuvab Artikli");
-                    removeAllButAside()
+
                     console.log(json.pealkiri);
                     buildFullArticle(json);
                };
@@ -190,11 +195,16 @@ $("#sisu").append(vasak,parem);
             footer.append(p2,loeKom,teeKom);
             article.append(footer);
             article.addClass("article");
+            //h1.removeClass("getArticle");
+            h1.addClass("getArticle");
 
 
                        var vp=$("<section id = 'vasakParem'></section>");
                         vp.append(article);
+                        removeAllButAside()
                        $("#sisu").append(vp);
+                       addAside()
+
 
                 }
 
@@ -204,5 +214,104 @@ $("#sisu").append(vasak,parem);
       console.log("eemaldab sisu");
        $(this).closest('article').remove();
       }); */
+
+
+
+           function addAside(){
+
+             console.log("aside");
+
+             var JSONaside1= {
+            "ArtikliPealkiri":"Esimene artikkel, id:2",
+            "ArtikliId":"2"};
+
+             var JSONaside2= {
+            "ArtikliPealkiri":"Teine artikkel, id:3",
+            "ArtikliId":3};
+
+              var JSONaside3= {
+            "ArtikliPealkiri":"Kolmas artikkel, id:4",
+            "ArtikliId":"4"};
+
+             var aside1 = $('<div id = "aside1"></div>');
+             var aside2 = $('<div id = "aside2"></div>');
+             var aside3 = $('<div id = "aside3"></div>');
+             var aside=$('<aside id = "side"></aside>');
+
+              $(aside).addClass(".side")
+              $(aside).append(aside1,aside2,aside3);
+              $('#sisu').append(aside);
+
+
+              // viimasedUudised
+
+              var aside1H4=$("<h4></h4>").text("Viimased uudised");
+              $("#aside1").append(aside1H4);
+            /*siia tuleb andmebaasi vaatest saadud artikli elemendi pealkiri:*/
+               var aside1H2=$('<h2 id="aside1H2"></h2>');
+              $("#aside1").append(aside1H2);
+                document.getElementById("aside1H2").innerHTML=JSONaside1.ArtikliPealkiri;
+                aside1H2.data("id",JSONaside1.ArtikliId);
+                //aside1H2.removeClass("getArticle");
+                aside1H2.addClass("getArticle");
+
+
+              //Enim loetud
+
+                var aside2H4=$("<h4></h4>").text("Enim loetud");
+              $("#aside2").append(aside2H4);
+                /*siia tuleb andmebaasi vaatest saadud artikli elemendi pealkiri:*/
+                 var aside2H2=$('<h2 id="aside2H2"></h2>');
+              $("#aside2").append(aside2H2);
+                document.getElementById("aside2H2").innerHTML=JSONaside2.ArtikliPealkiri;
+                aside2H2.data("id",JSONaside2.ArtikliId);
+                //aside2H2.removeClass("getArticle");
+                aside2H2.addClass("getArticle");
+
+
+              //Enim kommenteeritud
+
+                var aside3H4=$("<h4></h4>").text("Enim kommenteeritud");
+              $("#aside3").append(aside3H4);
+                 /*siia tuleb andmebaasi vaatest saadud artikli elemendi pealkiri:*/
+                 var aside3H2=$('<h2 id="aside3H2"></h2>');
+              $("#aside3").append(aside3H2);
+                document.getElementById("aside3H2").innerHTML=JSONaside3.ArtikliPealkiri;
+                aside3H2.data("id",JSONaside3.ArtikliId);
+                //aside3H2.removeClass("getArticle");
+                aside3H2.addClass("getArticle");
+
+
+           };
+
+           //see funktsioon peaks edasi viima uksiku artikli kuvamiseni, kui andmebaasist artikli json saadud
+               function vastus(data){
+                   console.log("siia peaks joudma andmebaasi vastus");
+                   console.log(data);
+                   //showArticle(data)
+               };
+
+               //kui vajutame artikli pealkirja peal (aside alal)
+               $(document).on("click", ".getArticle",function(){
+              // $(".getArticle").on('click',function(){
+                   console.log("valiti aside pealkiri");
+                    var id = $(this).data("id");
+                    console.log(id);
+                               // tahab andmebaasilt vastuseks vastava id-ga artiklit, json objektina
+                               $.ajax('/artiklid', {
+                                   type: "GET",
+                                   data: {id:id},
+                                   success: showArticle
+
+                               });
+
+
+                    console.log("-> paring: GET, '/artiklid', id: "+id);
+
+                    //lahendus ilma andmebaasita
+                    var data= JSONArticle[2]
+                    showArticle(data)
+
+               });
 
 });
