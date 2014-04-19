e2b161f6f45e30cff1291609f3959fc5cff67729
Kristiina Pokk <kristiinapokk@gmail.com>
2014-04-19 23:40:10 +0300

    Kommentaari servlet (teiste eeskujul tehtud) Vajab ülevaatamist, kui kasutusele võetakse

---
+++ src/main/java/ee/ut/math/vl/servlets/kommentaar/KommentaarController.java
@@ -0,0 +1,110 @@
+package ee.ut.math.vl.servlets.kommentaar;
+
+import com.google.gson.Gson;
+
+import ee.ut.math.vl.data.Artikkel;
+import ee.ut.math.vl.data.Kommentaar;
+import ee.ut.math.vl.datastore.artikkel.ArtikkelData;
+import ee.ut.math.vl.datastore.artikkel.ArtikkelDataProvider;
+import ee.ut.math.vl.datastore.kommentaar.KommentaarData;
+import ee.ut.math.vl.datastore.kommentaar.KommentaarDataProvider;
+import ee.ut.math.vl.servlets.artikkel.ArtikkelSocketController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by Kristiina on 19.04.14.
+ */
+
+
+@WebServlet(value = "/kommentaar")
+public class KommentaarController extends HttpServlet {
+
+
+    private static final long serialVersionUID = 1L;
+    private Gson gson;
+    private KommentaarDataProvider datastore;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        gson = new Gson();
+        datastore = new KommentaarData();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setHeader("Content-Type", "application/json");
+        resp.setContentType("text/html;charset=UTF-8");    //juurde
+        
+        Connection con = null ; //peab olema meetod, mis tekitab connectioneid (muide peabki olema väärtus null)
+        //try sees open connection
+        //finally : if con!=null close connection
+
+
+        String idString = req.getParameter("id");
+        if (idString != null) {
+            try {
+
+                replyWithKommentaarid(resp, idString);
+            }  catch (Exception ex) { throw new RuntimeException(ex); }
+
+        }
+        else {resp.sendError(HttpServletResponse.SC_BAD_REQUEST); }
+    }
+
+    private void replyWithKommentaarid(HttpServletResponse resp,
+                                         String idString) throws SQLException, Exception {
+        int id = Integer.parseInt(idString);
+
+
+        List<Kommentaar> kommentaar = datastore.findKommentaarByArtikkel(id);
+            try {
+            resp.getWriter().write(gson.toJson(kommentaar));
+
+        }
+
+        catch (Exception ex) { throw new RuntimeException(ex); }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try {
+
+
+
+            Kommentaar kommentaar = gson.fromJson(req.getReader(), Kommentaar.class);
+            datastore.lisaKommentaar(kommentaar);
+
+
+
+            String kommentaarEcho = gson.toJson(kommentaar);
+            resp.setHeader("Content-Type", "application/json");
+            resp.getWriter().write(kommentaarEcho);
+
+            // actually this is a bad place to send the broadcast.
+            // better: attach sockets as eventlisteners to the datastore
+            // even better: use message queues for servlet-datastore events
+
+            ArtikkelSocketController.find(req.getServletContext()).broadcast(
+                    kommentaarEcho);
+
+        } catch (Exception ex) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+        }
+    }
+
+
+}
