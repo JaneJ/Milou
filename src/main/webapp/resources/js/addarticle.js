2dfbdd54ba0d5cc9b250cccaf6a4d2ebe76c457e
JaneJ <jane193@hot.ee>
2014-04-23 23:36:11 +0300

    Katsetused info lisamiseks ilma file uplodita.

--- src/main/webapp/resources/js/addarticle.js
+++ src/main/webapp/resources/js/addarticle.js
@@ -20,19 +20,24 @@ $(document).ready(function(){
 	});
 });
 
-$(document).ready(function(){
+
 $('#submit').click(function() {
-        var Artikkel = new Object();
+	console.log('Data to DB');
+	var data=$("form.artikkel").serializeObject();
+	console.log(data);
+        var Artikkel = {};
+        //var Pilt = new Object();
+
+       // Pilt.pilt=$("#file").val();
         Artikkel.autor=$("#autor").val();
         Artikkel.pealkiri=$("#pealkiri").val();
-        Artikkel.pilt=$("#file").val();
         Artikkel.kirjeldus=$("#lyhikirjeldus").val();
         Artikkel.uudis=$("#sisu").val();
         Artikkel.teema = $('input:radio[name=teema]:checked').val();
         console.log(Artikkel.autor+ Artikkel.pealkiri+ Artikkel.kirjeldus);
        
  		if (Artikkel.teema=="Koomiks") {
- 			if (!Artikkel.pealkiri & !Artikkel.pilt & !Artikkel.pilt ) {
+ 			if (!Artikkel.pealkiri /* & !Pilt.pilt*/ ) {
  				alert("Kõik vajalikud väljad pole täidetud!");
  			}else {
 			$.ajax("/artiklid",{
@@ -44,10 +49,7 @@ $('#submit').click(function() {
  
 				success: function(Artikkel){   
 					console.log("success");
-					/*
-
-
-					*/
+				
 				},
 				error:function(req, text) {
 					console.log(req);
@@ -74,7 +76,7 @@ $('#submit').click(function() {
 				}
 
 			});
-		};});});
+		};});
 
 
 //Kuhugi siia funktsioon, mis lisab selle ka pealehele ???
