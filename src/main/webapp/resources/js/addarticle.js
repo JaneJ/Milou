11fc1b6727e2b416854cc77a6e8175aa1b611a5a
JaneJ <jane193@hot.ee>
2014-04-24 12:30:12 +0300

    Katse form to json.

--- src/main/webapp/resources/js/addarticle.js
+++ src/main/webapp/resources/js/addarticle.js
@@ -21,89 +21,41 @@ $(document).ready(function(){
 });
 
 
-/*$('#submit').click(function() {
-	console.log('Data to DB');
-	var data=$("form.artikkel").serializeObject();
-	var Art={};
-	var a=this.serializeArray();
-	
-	console.log(data);
-        var Artikkel = {};
-        //var Pilt = new Object();
+$('#submit').click(function(){
+	var radios=document.getElementByName('teema');
+	var teema='';
+	for(var i=0;i<radios.length;i++){
+   if(radios[i].checked)
+      teema = radios[i].value;
+	};
 
-       // Pilt.pilt=$("#file").val();
-        Artikkel.autor=$("#autor").val();
-        Artikkel.pealkiri=$("#pealkiri").val();
-        Artikkel.kirjeldus=$("#lyhikirjeldus").val();
-        Artikkel.uudis=$("#sisu").val();
-        Artikkel.teema = $('input:radio[name=teema]:checked').val();
-        console.log(Artikkel.autor+ Artikkel.pealkiri+ Artikkel.kirjeldus);
-       
- 		if (Artikkel.teema=="Koomiks") {
- 			if (!Artikkel.pealkiri & !Pilt.pilt) {
- 				alert("Kõik vajalikud väljad pole täidetud!");
- 			}else {
-			$.ajax("/artiklid",{
+
+	var JSONObject={
+		"autor": document.getElementById('autor'),
+		"pealkiri":document.getElementById('pealkiri'),
+		"kirjeldus":document.getElementById('lyhikirjeldus'),
+		"uudis":document.getElementById('sisu'),
+		"teema":teema
+		
+	}
+
+	//var json=JSON.stringify(JSONObject);
+	$.ajax("/artiklid",{
 				type:"POST",
 				
 				dataType:'json',
-				data: JSON.stringify(Artikkel),
+				data: JSON.stringify(JSONObject),
 				contentType: "application/json; charset=utf-8",
- 
-				success: function(Artikkel){   
+ 				success: function(Artikkel){   
 					console.log("success");
 				
 				},
 				error:function(req, text) {
 					console.log(req);
 					console.log(text);
-				}
+				}}
 
-			});}}
-		if (!Artikkel.teema & !Artikkel.autor & ! Artikkel.pealkiri & !Artikkel.kirjeldus & ! Artikkel.sisu ) {
-			alert("Kõik vajalikud väljad pole täidetud!")
-		}else {
-			$.ajax("/artiklid",{
-				type:"POST",
-				//url:"ArtikkelData/lisaArtikkel",
-				dataType:'json',
-				data: JSON.stringify(Artikkel),
-				contentType: "application/json; charset=utf-8",
- 
-				success: function(Artikkel){   
-					console.log("success");
-				},
-				error:function(req, text) {
-					console.log(req);
-					console.log(text);
-				}
-
-			});
-		};});*/
-
+});
 
 //Kuhugi siia funktsioon, mis lisab selle ka pealehele ???
 
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-
-$(function() {
-    $('artikkel').click(function() {
-        $('#result').text(JSON.stringify($('form').serializeObject()));
-        return false;
-    });
-});
