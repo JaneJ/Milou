194c3370279e9851ff26c5cab59add9f61f77b34
JaneJ <jane193@hot.ee>
2014-04-24 15:16:28 +0300

    Lisa Artikkel proov.

--- src/main/webapp/resources/js/addarticle.js
+++ src/main/webapp/resources/js/addarticle.js
@@ -18,13 +18,13 @@ $(document).ready(function(){
 		var pealkiri_alles=text_max-pealkiri_len;
 		$('#formMargidLuhi').html(pealkiri_alles+ ' märke alles.');
 	});
-});
 
+console.log("tere");
 
-$('#submit').click(function() {
+$('#lisaArtikkel').submit(function() {
 	console.log('Data to DB');
-	var data=$("form.artikkel").serializeObject();
-	console.log(data);
+	/*var data=$("form.artikkel").serializeObject();
+	console.log(data);*/
         var Artikkel = {};
         //var Pilt = new Object();
 
@@ -34,32 +34,14 @@ $('#submit').click(function() {
         Artikkel.kirjeldus=$("#lyhikirjeldus").val();
         Artikkel.uudis=$("#sisu").val();
         Artikkel.teema = $('input:radio[name=teema]:checked').val();
-        console.log(Artikkel.autor+ Artikkel.pealkiri+ Artikkel.kirjeldus);
+        //console.log(Artikkel.autor+ Artikkel.pealkiri+ Artikkel.kirjeldus);
        
- 		if (Artikkel.teema=="Koomiks") {
- 			if (!Artikkel.pealkiri /* & !Pilt.pilt*/ ) {
- 				alert("Kõik vajalikud väljad pole täidetud!");
- 			}else {
-			$.ajax("/artiklid",{
-				type:"POST",
-				
-				dataType:'json',
-				data: JSON.stringify(Artikkel),
-				contentType: "application/json; charset=utf-8",
- 
-				success: function(Artikkel){   
-					console.log("success");
-				
-				},
-				error:function(req, text) {
-					console.log(req);
-					console.log(text);
-				}
-
-			});}}
+ 		
 		if (!Artikkel.teema & !Artikkel.autor & ! Artikkel.pealkiri & !Artikkel.kirjeldus & ! Artikkel.sisu ) {
 			alert("Kõik vajalikud väljad pole täidetud!")
 		}else {
+
+			console.log(Artikkel);
 			$.ajax("/artiklid",{
 				type:"POST",
 				//url:"ArtikkelData/lisaArtikkel",
@@ -76,7 +58,10 @@ $('#submit').click(function() {
 				}
 
 			});
-		};});
+		}
+		return false;
+	});
 
+});
 
 //Kuhugi siia funktsioon, mis lisab selle ka pealehele ???
