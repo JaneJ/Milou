180bab77b102e0e49504e805d2f5a76e1b2e9162
JaneJ <jane193@hot.ee>
2014-04-24 13:07:54 +0300

    ..Veel katsetusi.

--- src/main/webapp/resources/js/addarticle.js
+++ src/main/webapp/resources/js/addarticle.js
@@ -20,9 +20,72 @@ $(document).ready(function(){
 	});
 });
 
+/*
+$('#submit').click(function() {
+	event.preventDefault();
+	console.log('Data to DB');
+	var data=$("form.artikkel").serializeObject();
+		
+	console.log(data);
+        var Artikkel = new Object();
+        //var Pilt = new Object();
+
+       // Pilt.pilt=$("#file").val();
+        Artikkel.autor=$("#autor").val();
+        Artikkel.pealkiri=$("#pealkiri").val();
+        Artikkel.kirjeldus=$("#lyhikirjeldus").val();
+        Artikkel.uudis=$("#sisu").val();
+        Artikkel.teema = $('input:radio[name=teema]:checked').val();
+        console.log(Artikkel.autor+ Artikkel.pealkiri+ Artikkel.kirjeldus);
+       
+ 		if (Artikkel.teema=="Koomiks") {
+ 			if (!Artikkel.pealkiri & /*!Pilt.pilt*//*) {
+ 				alert("Kõik vajalikud väljad pole täidetud!");
+ 			}else {
+			$.ajax("/artiklid",{
+				type:"POST",
+				
+				dataType:'json',
+				data: JSON.stringify(Artikkel),
+				contentType: "application/json; charset=utf-8",
+ 
+				success: function(Artikkel){   
+					console.log("success");
+				
+				},
+				error:function(req, text) {
+					console.log(req);
+					console.log(text);
+				}
+
+			});}}
+		if (!Artikkel.teema & !Artikkel.autor & ! Artikkel.pealkiri & !Artikkel.kirjeldus & ! Artikkel.sisu ) {
+			alert("Kõik vajalikud väljad pole täidetud!")
+		}else {
+			$.ajax("/artiklid",{
+				type:"POST",
+				//url:"ArtikkelData/lisaArtikkel",
+				dataType:'json',
+				data: JSON.stringify(Artikkel),
+				contentType: "application/json; charset=utf-8",
+ 
+				success: function(Artikkel){   
+					console.log("success");
+				},
+				error:function(req, text) {
+					console.log(req);
+					console.log(text);
+				}
+
+			});
+		};});*/
+
+
+
+
 
 $('#submit').click(function(){
-
+	event.preventDefault();
 	var radios=document.getElementByName('teema');
 	var teema='';
 	for(var i=0;i<radios.length;i++){
