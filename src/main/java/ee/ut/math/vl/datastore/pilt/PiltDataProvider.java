05af5dc1342dd991d1b52c0bed7d469221689bf8
Kristiina Pokk <kristiinapokk@gmail.com>
2014-05-05 14:58:11 +0300

    pildi kustutamine

--- src/main/java/ee/ut/math/vl/datastore/pilt/PiltDataProvider.java
+++
@@ -1,9 +0,0 @@
-package ee.ut.math.vl.datastore.pilt;
-
-import java.sql.SQLException;
-import ee.ut.math.vl.data.Pilt;
-
-public interface PiltDataProvider {
-	public Pilt findPiltByNimi(String nimi) throws SQLException, Exception;
-
-}
