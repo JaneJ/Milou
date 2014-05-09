fafc9148528863b78958e1db67fdf7ec97251c86
JaneJ <jane193@hot.ee>
2014-05-09 03:33:06 +0300

    Üleliigsed sulud.

--- src/main/webapp/resources/js/logins.js
+++ src/main/webapp/resources/js/logins.js
@@ -65,9 +65,7 @@ window.fbAsyncInit = function() {
     );
 }
 
-            });
-        }
-
+            
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
