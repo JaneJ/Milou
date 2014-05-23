e4e8ccd57a4c182929e6f422a25eebe5643a2071
clk <careelika@gmail.com>
2014-05-23 10:23:56 +0300

    Revert "Another js file to optimize."
    This reverts commit 4bb5c68cac68d12d07ed3674f9ec95c8775b037b.

--- src/main/webapp/resources/js/addcomment.js
+++ src/main/webapp/resources/js/addcomment.js
@@ -1,22 +1,43 @@
 $(document).ready(function(){
 console.log("teine js fail");
+//$('.addComment').submit(function(){
 	$("[class^='.comForm']").submit(function(){
+		console.log('Comment to DB');
 		var Kommentaar={};
 		Kommentaar.kommentaar=$('.kommentaar').val();
+        console.log("vaartus "+Kommentaar.kommentaar);
 		Kommentaar.artikkel=$(this).data("id");
+        console.log("kom vaartus "+Kommentaar.artikkel);
         Kommentaar.nimi=$(document).data('loggedName');
+        console.log("kom nimi "+Kommentaar.nimi);
 
+	/*	if(document.getElementById('anonuumne').checked){
+			Kommentaar.nimi="Anonüümne";
+		}else{
+			Kommentaar.nimi="";
+		}
+*/
 		$.ajax("/kommentaar",{
 				type:"POST",
+				//url:"ArtikkelData/lisaArtikkel",
 				dataType:'json',
 				data: JSON.stringify(Kommentaar),
 				contentType: "application/json; charset=utf-8",
-				success: function(Kommentaar){
+ 
+				success: function(Kommentaar){   
+					console.log("success");
 					alert('Kommentaar edukalt lisatud. ');
+					//document.location.reload(true);
 				},
 				error:function(req, text) {
+					console.log(req);
+					console.log(text);
+                    console.log("addcomment.js rida 35");
                     alert('Midagi on valesti addcomment.js rida 36 ');
 				}
+
 			});
+
 	});
+
 });
